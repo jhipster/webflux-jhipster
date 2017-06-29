d37bb4bd99593505672acc8e9924174e9450469a
JoffreyDcu <joffrey.derchu@hotmail.fr>
2017-06-29 18:10:52 +0200

    Tests on BankAccount MockMvc replaced by WebTestClient. All tests pass, but a few errors.

--- reactiveMongoDB/src/test/java/jhipster/reactive/web/rest/BankAccountResourceIntTest.java
+++ reactiveMongoDB/src/test/java/jhipster/reactive/web/rest/BankAccountResourceIntTest.java
@@ -1,11 +1,9 @@
 package jhipster.reactive.web.rest;
 
 import jhipster.reactive.ReporeactiveMongoDbApp;
-
 import jhipster.reactive.domain.BankAccount;
 import jhipster.reactive.repository.BankAccountRepository;
 import jhipster.reactive.web.rest.errors.ExceptionTranslator;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -16,22 +14,21 @@ import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.util.function.Tuple2;
 
-import javax.validation.constraints.AssertTrue;
-import java.io.IOException;
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.hasItem;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
+import static org.springframework.http.MediaType.TEXT_EVENT_STREAM;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -62,7 +59,7 @@ public class BankAccountResourceIntTest {
     @Autowired
     private ExceptionTranslator exceptionTranslator;
 
-    private MockMvc restBankAccountMockMvc;
+    private WebTestClient client;
 
     private BankAccount bankAccount;
 
@@ -70,10 +67,9 @@ public class BankAccountResourceIntTest {
     public void setup() {
         MockitoAnnotations.initMocks(this);
         BankAccountResource bankAccountResource = new BankAccountResource(bankAccountRepository);
-        this.restBankAccountMockMvc = MockMvcBuilders.standaloneSetup(bankAccountResource)
-            .setCustomArgumentResolvers(pageableArgumentResolver)
-            .setControllerAdvice(exceptionTranslator)
-            .setMessageConverters(jacksonMessageConverter).build();
+        this.client = WebTestClient
+            .bindToController(bankAccountResource)
+            .build();
     }
 
     /**
@@ -98,19 +94,14 @@ public class BankAccountResourceIntTest {
     @Test
     public void createBankAccount() throws Exception {
         Mono<Integer> databaseSizeBeforeCreate = bankAccountRepository.findAll().collectList().map((List list)->list.size());
-        databaseSizeBeforeCreate.doOnTerminate(( bankAccount1, throwable) -> {
-                assertNull(throwable);
 
-                // Create the BankAccount
-            try{
-                restBankAccountMockMvc.perform(post("/api/bank-accounts")
-                    .contentType(TestUtil.APPLICATION_JSON_UTF8)
-                    .content(TestUtil.convertObjectToJsonBytes(bankAccount)))
-                    .andExpect(status().isCreated());
-            } catch (Exception e){
-                throw new RuntimeException();
-            }
-            });
+        // Create the BankAccount
+        this.client.put().uri("/api/bank-accounts")
+            .contentType(TestUtil.APPLICATION_JSON_UTF8)
+            .syncBody(TestUtil.convertObjectToJsonBytes(bankAccount))
+            .exchange()
+            .expectStatus().isCreated();
+
         // Validate the BankAccount in the database
         Flux<BankAccount> bankAccountList = bankAccountRepository.findAll();
         Mono.when(bankAccountList.collectList().map((List list)->list.size()),databaseSizeBeforeCreate)
@@ -121,8 +112,8 @@ public class BankAccountResourceIntTest {
 
         Mono<BankAccount> testBankAccount = bankAccountList.last();
         testBankAccount.map((BankAccount lastBankAccount)->{
-            assertThat(lastBankAccount.getName()).isEqualTo(UPDATED_NAME);
-            assertThat(lastBankAccount.getBalance()).isEqualTo(UPDATED_BALANCE);
+            assertThat(lastBankAccount.getName()).isEqualTo(DEFAULT_NAME);
+            assertThat(lastBankAccount.getBalance()).isEqualTo(DEFAULT_BALANCE);
             return null;
         }).subscribe();
 
@@ -135,18 +126,13 @@ public class BankAccountResourceIntTest {
         // Create the BankAccount with an existing ID
         bankAccount.setId("existing_id");
 
-        databaseSizeBeforeCreate.doOnTerminate(( bankAccount1, throwable) -> {
-            assertNull(throwable);
-            try{
-            // An entity with an existing ID cannot be created, so this API call must fail
-            restBankAccountMockMvc.perform(post("/api/bank-accounts")
-                .contentType(TestUtil.APPLICATION_JSON_UTF8)
-                .content(TestUtil.convertObjectToJsonBytes(bankAccount)))
-                .andExpect(status().isBadRequest());
-            } catch (Exception e){
-                throw new RuntimeException();
-            }
-        });
+        // An entity with an existing ID cannot be created, so this API call must fail
+        this.client.post().uri("/api/bank-accounts")
+            .contentType(TestUtil.APPLICATION_JSON_UTF8)
+            .syncBody(TestUtil.convertObjectToJsonBytes(bankAccount))
+            .exchange()
+            .expectStatus().isBadRequest();
+
         // Validate the Alice in the database
         Flux<BankAccount> bankAccountList = bankAccountRepository.findAll();
         Mono.when(bankAccountList.collectList().map((List list)->list.size()),databaseSizeBeforeCreate)
@@ -164,10 +150,11 @@ public class BankAccountResourceIntTest {
 
         // Create the BankAccount, which fails.
 
-        restBankAccountMockMvc.perform(post("/api/bank-accounts")
+        this.client.post().uri("/api/bank-accounts")
             .contentType(TestUtil.APPLICATION_JSON_UTF8)
-            .content(TestUtil.convertObjectToJsonBytes(bankAccount)))
-            .andExpect(status().isBadRequest());
+            .syncBody(TestUtil.convertObjectToJsonBytes(bankAccount))
+            .exchange()
+            .expectStatus().isBadRequest();
 
         Flux<BankAccount> bankAccountList = bankAccountRepository.findAll();
         Mono.when(bankAccountList.collectList().map((List list)->list.size()),databaseSizeBeforeTest)
@@ -184,11 +171,11 @@ public class BankAccountResourceIntTest {
         bankAccount.setBalance(null);
 
         // Create the BankAccount, which fails.
-
-        restBankAccountMockMvc.perform(post("/api/bank-accounts")
+        this.client.post().uri("/api/bank-accounts")
             .contentType(TestUtil.APPLICATION_JSON_UTF8)
-            .content(TestUtil.convertObjectToJsonBytes(bankAccount)))
-            .andExpect(status().isBadRequest());
+            .syncBody(TestUtil.convertObjectToJsonBytes(bankAccount))
+            .exchange()
+            .expectStatus().isBadRequest();
 
         Flux<BankAccount> bankAccountList = bankAccountRepository.findAll();
         Mono.when(bankAccountList.collectList().map((List list)->list.size()),databaseSizeBeforeTest)
@@ -201,154 +188,134 @@ public class BankAccountResourceIntTest {
     @Test
     public void getAllBankAccounts() throws Exception {
         // Initialize the database
-        bankAccountRepository.save(bankAccount).doOnTerminate((BankAccount bankAccount1, Throwable throwable) -> {
-            assertNull(throwable);
+        bankAccountRepository.deleteAll().block();
+        bankAccountRepository.save(bankAccount).subscribe();
+        ArrayList<BankAccount> bankAccountArrayList = new ArrayList<>(1);
+        bankAccountArrayList.add(bankAccount);
 
-            // Get all the bankAccountList
-            try {
-                restBankAccountMockMvc.perform(get("/api/bank-accounts?sort=id,desc"))
-                    .andExpect(status().isOk())
-                    .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
-                    .andExpect(jsonPath("$.[*].id").value(hasItem(bankAccount.getId())))
-                    .andExpect(jsonPath("$.[*].name").value(hasItem(DEFAULT_NAME.toString())))
-                    .andExpect(jsonPath("$.[*].balance").value(hasItem(DEFAULT_BALANCE.intValue())));
-            } catch (Exception e) {
-                throw new RuntimeException();
-            }
-        });
+        // Get all the bankAccountList
+        this.client.get().uri("/api/bank-accounts?sort=id,desc")
+            .exchange()
+            .expectStatus().isOk()
+            .expectHeader().contentType(TestUtil.APPLICATION_JSON_UTF8)
+            .expectBodyList(BankAccount.class)
+            .isEqualTo(bankAccountArrayList);
     }
 
     @Test
     public void getBankAccount() throws Exception {
         // Initialize the database
-        bankAccountRepository.save(bankAccount).doOnTerminate((BankAccount bankAccount1, Throwable throwable) -> {
-            assertNull(throwable);
+        bankAccountRepository.deleteAll().block();
+        bankAccountRepository.save(bankAccount).subscribe();
+        ArrayList<BankAccount> bankAccountArrayList = new ArrayList<>(1);
+        bankAccountArrayList.add(bankAccount);
 
-            // Get the bankAccount
-            try{
-            restBankAccountMockMvc.perform(get("/api/bank-accounts/{id}", bankAccount.getId()))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
-                .andExpect(jsonPath("$.id").value(bankAccount.getId()))
-                .andExpect(jsonPath("$.name").value(DEFAULT_NAME.toString()))
-                .andExpect(jsonPath("$.balance").value(DEFAULT_BALANCE.intValue()));
-            } catch (Exception e){
-                throw new RuntimeException();
-            }
-        });
+        // Get the bankAccount
+        this.client.get().uri("/api/bank-accounts/{id}", bankAccount.getId())
+            .exchange()
+            .expectStatus().isOk()
+            .expectHeader().contentType(TestUtil.APPLICATION_JSON_UTF8)
+            .expectBodyList(BankAccount.class)
+            .isEqualTo(bankAccountArrayList);
     }
 
     @Test
     public void getNonExistingBankAccount() throws Exception {
         // Get the bankAccount
-        restBankAccountMockMvc.perform(get("/api/bank-accounts/{id}", Long.MAX_VALUE))
-            .andExpect(status().isNotFound());
+        this.client.get().uri("/api/bank-accounts/{id}", Long.MAX_VALUE)
+            .exchange()
+            .expectStatus().isNotFound();
     }
 
     @Test
     public void updateBankAccount() throws Exception {
         // Initialize the database
-        bankAccountRepository.save(bankAccount).doOnTerminate((BankAccount bankAccount1,Throwable throwable)->{
-            assertNull(throwable);
-            Mono<Integer> databaseSizeBeforeUpdate = bankAccountRepository.findAll().collectList().map((List list)->list.size());
+        Mono<BankAccount> updatedBankAccount = bankAccountRepository.save(bankAccount);
+        updatedBankAccount.subscribe();
+        Mono<Integer> databaseSizeBeforeUpdate = bankAccountRepository.findAll().collectList().map((List list)->list.size());
 
-            // Update the bankAccount
-            Mono<BankAccount> updatedBankAccount = bankAccountRepository.findById(bankAccount.getId());
-            updatedBankAccount
-                .hasElement()
-                .map((Boolean bool) -> {
+        // Update the bankAccount
+        updatedBankAccount.hasElement()
+            .map((Boolean bool) -> {
                 assertTrue(bool);
                 return null;
-            }).subscribe();
-            updatedBankAccount
-                .map((BankAccount savedBankAccount)->{
-                    savedBankAccount.setName(UPDATED_NAME);
-                    savedBankAccount.setBalance(UPDATED_BALANCE);
-                    return savedBankAccount;
-                })
-                .subscribe();
-
-            updatedBankAccount.map((BankAccount savedBankAccount)->{
-                try{
-                restBankAccountMockMvc.perform(put("/api/bank-accounts")
+            })
+            .subscribe();
+        updatedBankAccount.map((BankAccount savedBankAccount)->{
+            savedBankAccount.setId(null);
+            savedBankAccount.setName(UPDATED_NAME);
+            savedBankAccount.setBalance(UPDATED_BALANCE);
+            try{
+                this.client.put().uri("/api/bank-accounts")
                     .contentType(TestUtil.APPLICATION_JSON_UTF8)
-                    .content(TestUtil.convertObjectToJsonBytes(savedBankAccount)))
-                    .andExpect(status().isOk());
-                } catch (Exception e){
-                    throw new RuntimeException();
-                }
-                return null;
-            }).subscribe();
+                    .syncBody(TestUtil.convertObjectToJsonBytes(savedBankAccount))
+                    .exchange()
+                    .expectStatus().isCreated();
+            } catch (Exception e){
+                throw new RuntimeException();
+            }
+            return null;
+        }).subscribe();
 
-            // Validate the BankAccount in the database
-            Flux<BankAccount> bankAccountList = bankAccountRepository.findAll();
-            Mono.when(bankAccountList.collectList().map((List list)->list.size()),databaseSizeBeforeUpdate)
-                .map((Tuple2 tuple)->{
-                    assertTrue(tuple.getT1().equals(tuple.getT2()));
-                    return null;
-                })
-                .subscribe();
-
-            Mono<BankAccount> testBankAccount = bankAccountList.last();
-            testBankAccount.map((BankAccount lastBankAccount)->{
-                assertThat(lastBankAccount.getName()).isEqualTo(UPDATED_NAME);
-                assertThat(lastBankAccount.getBalance()).isEqualTo(UPDATED_BALANCE);
+        // Validate the BankAccount in the database
+        Flux<BankAccount> bankAccountList = bankAccountRepository.findAll();
+        Mono.when(bankAccountList.collectList().map((List list)->list.size()),databaseSizeBeforeUpdate)
+            .map((Tuple2 tuple)->{
+                assertTrue(tuple.getT1().equals(tuple.getT2()));
                 return null;
-            }).subscribe();
-        });
+            })
+            .subscribe();
+
+        Mono<BankAccount> testBankAccount = bankAccountList.last();
+        testBankAccount.map((BankAccount lastBankAccount)->{
+            assertThat(lastBankAccount.getName()).isEqualTo(UPDATED_NAME);
+            assertThat(lastBankAccount.getBalance()).isEqualTo(UPDATED_BALANCE);
+            return null;
+        }).subscribe();
     }
 
     @Test
     public void updateNonExistingBankAccount() throws Exception {
         Mono<Integer> databaseSizeBeforeUpdate = bankAccountRepository.findAll().collectList().map((List list)->list.size());
-        databaseSizeBeforeUpdate.doOnTerminate((bankAccount1,throwable)-> {
-            // Create the BankAccount
-            assertNull(throwable);
-            // If the entity doesn't have an ID, it will be created instead of just being updated
-            try{
-                restBankAccountMockMvc.perform(put("/api/bank-accounts")
-                .contentType(TestUtil.APPLICATION_JSON_UTF8)
-                .content(TestUtil.convertObjectToJsonBytes(bankAccount)))
-                .andExpect(status().isCreated());
-             } catch (Exception e){
-                throw new RuntimeException();
-             }
-            // Validate the BankAccount in the database
-            Flux<BankAccount> bankAccountList = bankAccountRepository.findAll();
-            Mono.when(bankAccountList.collectList().map((List list) -> list.size()), databaseSizeBeforeUpdate)
-                .map((Tuple2 tuple) -> {
-                    assertTrue(tuple.getT1().equals((Integer) tuple.getT2() + 1));
-                    return null;
-                })
-                .subscribe();
-        });
+        // Create the BankAccount
+        // If the entity doesn't have an ID, it will be created instead of just being updated
+        this.client.put().uri("/api/bank-accounts")
+            .contentType(TestUtil.APPLICATION_JSON_UTF8)
+            .syncBody(TestUtil.convertObjectToJsonBytes(bankAccount))
+            .exchange()
+            .expectStatus().isCreated();
+
+        // Validate the BankAccount in the database
+        Flux<BankAccount> bankAccountList = bankAccountRepository.findAll();
+        Mono.when(bankAccountList.collectList().map((List list) -> list.size()), databaseSizeBeforeUpdate)
+            .flatMap((Tuple2 tuple) -> {
+                assertTrue(tuple.getT1().equals((Integer) tuple.getT2() + 1));
+                return null;
+            })
+            .subscribe();
+
     }
 
     @Test
     public void deleteBankAccount() throws Exception {
         // Initialize the database
-        bankAccountRepository.save(bankAccount).doOnTerminate((BankAccount bankAccount1,Throwable throwable)-> {
-            assertNull(throwable);
-            Mono<Integer> databaseSizeBeforeDelete = bankAccountRepository.findAll().collectList().map((List list) -> list.size());
+        bankAccountRepository.deleteAll().block();
+        bankAccountRepository.save(bankAccount).subscribe();
+        Mono<Integer> databaseSizeBeforeDelete = bankAccountRepository.findAll().collectList().map((List list)->list.size());
 
-            // Get the bankAccount
-            try{
-            restBankAccountMockMvc.perform(delete("/api/bank-accounts/{id}", bankAccount.getId())
-                .accept(TestUtil.APPLICATION_JSON_UTF8))
-                .andExpect(status().isOk());
-            } catch (Exception e){
-                throw new RuntimeException();
-            }
+        // Get the bankAccount
+        this.client.delete().uri("/api/bank-accounts/{id}", bankAccount.getId())
+            .exchange()
+            .expectStatus().isOk();
 
-            // Validate the database is empty
-            Flux<BankAccount> bankAccountList = bankAccountRepository.findAll();
-            Mono.when(bankAccountList.collectList().map((List list) -> list.size()), databaseSizeBeforeDelete)
-                .map((Tuple2 tuple) -> {
-                    assertTrue(tuple.getT1().equals((Integer) tuple.getT2() + 1));
-                    return null;
-                })
-                .subscribe();
-        });
+        // Validate the database is empty
+        Flux<BankAccount> bankAccountList = bankAccountRepository.findAll();
+        Mono.when(bankAccountList.collectList().map((List list)->list.size()),databaseSizeBeforeDelete)
+            .flatMap((Tuple2 tuple)->{
+                assertTrue(tuple.getT1().equals((Integer)tuple.getT2()+1));
+                return null;
+            })
+            .subscribe();
     }
 
     @Test
