990e9b8f932fa16097a40bb51f0ef818cacde11f
JoffreyDcu <joffrey.derchu@hotmail.fr>
2017-07-05 11:50:20 +0200

    cassandra default spring4

---
+++ defaultCassandraSpring4/src/main/webapp/app/entities/label/label-dialog.component.ts
@@ -0,0 +1,111 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Response } from '@angular/http';
+
+import { Observable } from 'rxjs/Rx';
+import { NgbActiveModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import { JhiEventManager, JhiAlertService } from 'ng-jhipster';
+
+import { Label } from './label.model';
+import { LabelPopupService } from './label-popup.service';
+import { LabelService } from './label.service';
+
+@Component({
+    selector: 'jhi-label-dialog',
+    templateUrl: './label-dialog.component.html'
+})
+export class LabelDialogComponent implements OnInit {
+
+    label: Label;
+    authorities: any[];
+    isSaving: boolean;
+
+    constructor(
+        public activeModal: NgbActiveModal,
+        private alertService: JhiAlertService,
+        private labelService: LabelService,
+        private eventManager: JhiEventManager
+    ) {
+    }
+
+    ngOnInit() {
+        this.isSaving = false;
+        this.authorities = ['ROLE_USER', 'ROLE_ADMIN'];
+    }
+
+    clear() {
+        this.activeModal.dismiss('cancel');
+    }
+
+    save() {
+        this.isSaving = true;
+        if (this.label.id !== undefined) {
+            this.subscribeToSaveResponse(
+                this.labelService.update(this.label), false);
+        } else {
+            this.subscribeToSaveResponse(
+                this.labelService.create(this.label), true);
+        }
+    }
+
+    private subscribeToSaveResponse(result: Observable<Label>, isCreated: boolean) {
+        result.subscribe((res: Label) =>
+            this.onSaveSuccess(res, isCreated), (res: Response) => this.onSaveError(res));
+    }
+
+    private onSaveSuccess(result: Label, isCreated: boolean) {
+        this.alertService.success(
+            isCreated ? `A new Label is created with identifier ${result.id}`
+            : `A Label is updated with identifier ${result.id}`,
+            null, null);
+
+        this.eventManager.broadcast({ name: 'labelListModification', content: 'OK'});
+        this.isSaving = false;
+        this.activeModal.dismiss(result);
+    }
+
+    private onSaveError(error) {
+        try {
+            error.json();
+        } catch (exception) {
+            error.message = error.text();
+        }
+        this.isSaving = false;
+        this.onError(error);
+    }
+
+    private onError(error) {
+        this.alertService.error(error.message, null, null);
+    }
+}
+
+@Component({
+    selector: 'jhi-label-popup',
+    template: ''
+})
+export class LabelPopupComponent implements OnInit, OnDestroy {
+
+    modalRef: NgbModalRef;
+    routeSub: any;
+
+    constructor(
+        private route: ActivatedRoute,
+        private labelPopupService: LabelPopupService
+    ) {}
+
+    ngOnInit() {
+        this.routeSub = this.route.params.subscribe((params) => {
+            if ( params['id'] ) {
+                this.modalRef = this.labelPopupService
+                    .open(LabelDialogComponent, params['id']);
+            } else {
+                this.modalRef = this.labelPopupService
+                    .open(LabelDialogComponent);
+            }
+        });
+    }
+
+    ngOnDestroy() {
+        this.routeSub.unsubscribe();
+    }
+}
